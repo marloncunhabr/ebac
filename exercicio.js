2f68d0a5a03b6189ded77fb67697dda6984111b2
Marlon da Cunha <103441453+marloncunhabr@users.noreply.github.com>
2023-08-21 16:38:22 -0300

    exercicio

--- exercicio.js
+++ exercicio.js
@@ -1,11 +1,52 @@
-"use strict";
-function multiplicacao(numero1, numero2) {
-    return numero1 * numero2;
+// Classe abstrata
+class Veiculo {
+    constructor(marca, modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    acelerar() {
+        console.log(`${this.modelo} está acelerando...`);
+    }
+
+    frear() {
+        console.log(`${this.modelo} está freando...`);
+    }
 }
-function saudacao(nome) {
-    return `Olá ${nome}`;
+
+// Classes herdeiras
+class Carro extends Veiculo {
+    constructor(marca, modelo, portas) {
+        super(marca, modelo);
+        this.portas = portas;
+    }
+
+    abrirPortas() {
+        console.log(`${this.modelo} está abrindo as portas...`);
+    }
 }
-const resultadoMultiplicacao = multiplicacao(5, 3);
-console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
-const saudacaoFinal = saudacao("Alice");
-console.log(saudacaoFinal);
+
+class Moto extends Veiculo {
+    constructor(marca, modelo, cilindradas) {
+        super(marca, modelo);
+        this.cilindradas = cilindradas;
+    }
+
+    empinar() {
+        console.log(`${this.modelo} está empinando a moto!`);
+    }
+}
+
+// Instâncias de objetos
+const carro1 = new Carro("Toyota", "Corolla", 4);
+const moto1 = new Moto("Honda", "CBR500R", 500);
+const carro2 = new Carro("Ford", "Mustang", 2);
+
+carro1.acelerar();
+carro1.abrirPortas();
+
+moto1.acelerar();
+moto1.empinar();
+
+carro2.frear();
+carro2.abrirPortas();
